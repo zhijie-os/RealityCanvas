d1a13fee820bebbb1e74f9c208d184cbfe293564
Ryo Suzuki <ryosuzk@gmail.com>
2022-02-22 22:15:27 -0700

    Refactor the code

--- src/Aframe.js
+++ src/Aframe.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
+import AFRAME from 'aframe'
 import Canvas from './Canvas'
 import Event from './Event'
-
-import AFRAME from 'aframe'
-
 
 class Aframe extends Component {
   constructor(props) {
@@ -13,54 +11,47 @@ class Aframe extends Component {
   }
 
   componentDidMount() {
-
-    AFRAME.registerComponent('startButton', {
-     init: function () {
-        var cube = document.querySelector("#boxId");
-        cube.addEventListener("mousedown",function(evt){
-          cube.setAttribute('material', 'color',"red");
-        });
-        cube.addEventListener("mouseup",function(evt){
-          cube.setAttribute('material', 'color',"blue");
-        });
-     }
-    });
-    
     AFRAME.registerComponent('drawable', {
       init: function() {
-        let mesh = this.el.object3D.children[0]
-        console.log(mesh)
-
         this.event = new Event()
-
-        let size = 1024
-        let canvas = document.getElementById('konva-1')
-        canvas.width = canvas.height = size
-        let texture = new THREE.Texture(canvas)
-        console.log(texture)
+        this.mesh = this.el.object3D.children[0]
+        this.size = 1024
+        this.canvas = document.getElementById('konva-1')
+        this.canvas.width = this.canvas.height = this.size
+        let texture = new THREE.Texture(this.canvas)
         let material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide })
-        mesh.material = material
-        window.mesh = mesh
-        mesh.material.transparent = true
-        this.mesh = mesh
-
+        this.mesh.material = material
+        this.mesh.material.transparent = true
         this.camera = document.getElementById('camera')
-        this.threeCamera = this.camera.getObject3D('camera')
         this.internalState = {
           dragging: false,
           initDrawing: true,
           distance: 0,
           raycaster: new THREE.Raycaster(),
         }
-        this.fingerMove = this.fingerMove.bind(this)
-        this.fingerUp = this.fingerUp.bind(this)
-        this.el.sceneEl.addEventListener('onefingermove', this.fingerMove)
-        this.el.sceneEl.addEventListener('onefingerend', this.fingerUp)
-        this.el.classList.add('cantap')
+        this.el.sceneEl.addEventListener('mousedown', this.mouseDown.bind(this))
+        this.el.sceneEl.addEventListener('mousemove', this.mouseMove.bind(this))
+        this.el.sceneEl.addEventListener('mouseup', this.mouseUp.bind(this))
+        this.el.sceneEl.addEventListener('onefingermove', this.fingerMove.bind(this))
+        this.el.sceneEl.addEventListener('onefingerend', this.fingerUp.bind(this))
+      },
+
+      mouseDown(event) {
+        this.internalState.dragging = true
+      },
+
+      mouseMove(event) {
+        this.internalState.mouse = { x: event.clientX, y: event.clientY }
+      },
+
+      mouseUp(event) {
+        this.internalState.dragging = false
+        this.internalState.initDrawing = true
+        this.event.mouseUp()
       },
 
       fingerMove(event) {
-        this.internalState.positionRaw = event.detail.positionRaw
+        this.internalState.mouse = { x: event.clientX, y: event.clientY }
         this.internalState.dragging = true
       },
 
@@ -73,30 +64,27 @@ class Aframe extends Component {
       tick: function() {
         this.mesh.material.map.needsUpdate = true
         if (this.internalState.dragging) {
-          const screenPositionX = this.internalState.positionRaw.x / document.body.clientWidth * 2 - 1
-          const screenPositionY = this.internalState.positionRaw.y / document.body.clientHeight * 2 - 1
+          const screenPositionX = this.internalState.mouse.x / window.innerWidth * 2 - 1
+          const screenPositionY = this.internalState.mouse.y / window.innerHeight * 2 - 1
           const screenPosition = new THREE.Vector2(screenPositionX, -screenPositionY)
 
-          this.threeCamera = this.threeCamera || this.camera.getObject3D('camera')
+          this.threeCamera = this.camera.getObject3D('camera')
           this.internalState.raycaster.setFromCamera(screenPosition, this.threeCamera)
           const intersects = this.internalState.raycaster.intersectObject(this.el.object3D, true)
-
           if (intersects.length > 0) {
             const intersect = intersects[0]
             this.internalState.distance = intersect.distance
             let point = intersect.point
             let mouse = {
-              x: 1024 * intersect.uv.x,
-              y: 1024 * (1- intersect.uv.y)
+              x: this.size * intersect.uv.x,
+              y: this.size * (1- intersect.uv.y)
             }
-            console.log(mouse)
             if (this.internalState.initDrawing) {
               this.event.mouseDown(mouse)
               this.internalState.initDrawing = false
             } else {
               this.event.mouseMove(mouse)
             }
-
           }
         }
       }
@@ -118,7 +106,7 @@ class Aframe extends Component {
               fuse: false;
               rayOrigin: mouse;"
           >
-           </a-camera>
+          </a-camera>
 
           <a-plane
             id="plane"
--- src/Event.js
+++ src/Event.js
@@ -1,188 +1,48 @@
 class Event {
 
   constructor() {
-    this.stage = Canvas.stage
-    this.layer = Canvas.layer
-    this.lineLayer = Canvas.lineLayer
-    // this.lastLine = canvas.lastLine
-    this.physics = Canvas.physics
-    this.parameterize = Canvas.parameterize
-    this.graph = Canvas.graph
+    this.canvas = window.Canvas
+    this.lineLayer = this.canvas.lineLayer
     this.isPaint = false
-    this.mode = null
-    this.anim = null
-    this.time = null
-    this.animatedLineStorage = null
-    this.emitterLinePoints = []
   }
 
   mouseDown(pos) {
-    if (this.mode === 'parameterize') {
-      this.parameterize.mouseDown(pos)
-      return
-    }
-    if (this.mode === 'graph') {
-      this.graph.mouseDown(pos)
-      return
-    }
-    /*
-    if (e.target !== this.stage) return
     this.isPaint = true
-    let pos = this.stage.getPointerPosition()
     this.lastLine = new Konva.Line({
-      stroke: 'red',
+      stroke: (this.canvas.drawingMode === 'emitterLine') ? 'blue' : 'red',
       strokeWidth: 5,
       lineCap: 'round',
+      name:  (this.canvas.drawingMode === 'emitterLine') ? 'emitterLine' : 'lineToAnimate',
       points: [pos.x, pos.y, pos.x, pos.y]
     })
-    this.layer.add(this.lastLine)
-    */
-
-    this.isPaint = true
-
-
-
-    this.lastLine = new Konva.Line({
-      stroke: (( Canvas.drawingMode === "emitterLine") ? 'blue' : 'red'),
-      strokeWidth: 5,
-      lineCap: 'round',
-      name:  (( Canvas.drawingMode === "emitterLine") ? 'emitterLine' : 'lineToAnimate'),
-      points: [pos.x, pos.y, pos.x, pos.y]
-    })
-    this.layer.add(this.lastLine)
+    this.canvas.layer.add(this.lastLine)
     
-    if(Canvas.drawingMode === "emitterLine")
-    {
-      Canvas.emitterLinePointsCopy.push(pos.x, pos.y, pos.x, pos.y)
-      // console.log(typeof(pos.x))
-      // console.log(emitterLinePoints)
-
+    if(this.canvas.drawingMode === 'emitterLine') {
+      this.canvas.emitterLinePointsCopy.push(pos.x, pos.y, pos.x, pos.y)
     }
   }
 
   mouseMove(pos) {
-    if (this.mode === 'parameterize') {
-      this.parameterize.mouseMove(pos)
-      return
-    }
-    if (this.mode === 'graph') {
-      this.graph.mouseMove(pos)
-      return
-    }
     if (!this.isPaint) return
-    /*
-    e.evt.preventDefault()
-    const pos = this.stage.getPointerPosition()
-    let newPoints = this.lastLine.points().concat([pos.x, pos.y])
-    this.lastLine.points(newPoints)
-    */
-
-        if(Canvas.drawingMode === "emitterLine")
-    {
-      console.log("in emitter line")
-      Canvas.emitterLinePointsCopy.push(pos.x, pos.y)
-          let newPoints = this.lastLine.points().concat([pos.x, pos.y])
-    this.lastLine.points(newPoints)
-      // console.log(typeof(pos.x))
-      // console.log(emitterLinePoints)
-
-    }
-    else{
-    let newPoints = this.lastLine.points().concat([pos.x, pos.y])
-    this.lastLine.points(newPoints)
+    if(this.canvas.drawingMode === 'emitterLine') {
+      console.log('in emitter line')
+      this.canvas.emitterLinePointsCopy.push(pos.x, pos.y)
+      let newPoints = this.lastLine.points().concat([pos.x, pos.y])
+      this.lastLine.points(newPoints)
+    } else {
+      let newPoints = this.lastLine.points().concat([pos.x, pos.y])
+      this.lastLine.points(newPoints)
     }
   }
 
   mouseUp(pos) {
-    if (this.mode === 'parameterize') {
-      this.parameterize.mouseUp(pos)
-      return
-    }
-    if (this.mode === 'graph') {
-      this.graph.mouseUp(pos)
-      return
-    }
-
     if (!this.isPaint) return
-
     this.isPaint = false
-    Canvas.setState({ lastLine: this.lastLine })
-    if(Canvas.drawingMode === "emitterLine")
-    {
-      console.log("in emitter line")
-      Canvas.emitterLinePointsCopy.push(pos.x, pos.y)
-      // console.log(typeof(pos.x))
-      // console.log(emitterLinePoints)
-
-    }
-
-
-
-
-    //Canvas.morph.animate()
-  }
-
-  dragMove(e) {
-    let target = e.target
-    // console.log(target.x(), target.y())
-    if (this.ball && this.line) {
-      let body = window.mouseConstraint.body
-      window.mouseConstraint.pointA = {
-        x: target.x(),
-        y: target.y()
-      }
-      window.mouseConstraint.bodyB = body
-      window.mouseConstraint.pointB = {
-        x: target.x() - body.position.x,
-        y: target.y() - body.position.y
-      }
-      window.mouseConstraint.angleB = body.angle
-      Matter.Sleeping.set(body, false)
-      // window.mouseConstraint.bodyB = window.mouseConstraint.body
-      // renderConstraint()
-
-      let point0 = pathParse(this.line.getData()).result[0]
-      let x = point0[1] + this.line.x()
-      let y = point0[2] + this.line.y()
-      return
-    }
-
-    this.parameterize.updateAll()
-
-    // detect collision
-    let bb = e.target.getClientRect()
-    for (let child of this.layer.children) {
-      if (child === target) continue
-      let cb = child.getClientRect()
-      if (this.haveIntersection(cb, bb)) {
-        console.log('intersect')
-        this.line = child
-        this.ball = target
-        let point = pathParse(this.line.getData()).result[1]
-        // ['L', x, y]
-        let x = point[1] + this.line.x()
-        let y = point[2] + this.line.y()
-        this.ball.setAttr('x', x)
-        this.physics.addConstraint(this.line, this.ball)
-      }
+    this.canvas.setState({ lastLine: this.lastLine })
+    if(this.canvas.drawingMode === 'emitterLine') {
+      this.canvas.emitterLinePointsCopy.push(pos.x, pos.y)
     }
   }
-
-  dragEnd(e) {
-    if (!window.mouseConstraint) return
-    window.mouseConstraint.bodyB = null
-    window.mouseConstraint.pointB = null
-  }
-
-  haveIntersection(bb1, bb2) {
-    return !(
-      bb2.x > bb1.x + bb1.width ||
-      bb2.x + bb2.width < bb1.x ||
-      bb2.y > bb1.y + bb1.height ||
-      bb2.y + bb2.height < bb1.y
-    )
-  }
-
 
 }
 
